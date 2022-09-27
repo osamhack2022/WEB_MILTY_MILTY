9f01548e7b590e8c10f05e353c8e643706f5d552
SoonHyung_Lee <tnsgud9@naver.com>
2022-09-27 02:09:49 +0000

    chore: @types/passport 추가, 디렉토리 구조 변경, 로그인 기능 ts로 변환
    - passport @types 패키지를 추가했습니다.
    - 디렉토리 각 기능에 맞게 분리하였습니다.
    - 로그인 기능들을 ts로 변경했습니다.
    
    ※ 다만 스크립트가 미완성인 부분이 있어 해당 커밋에선 실행 불가합니다.

--- WEB(BE)/src/middleware/passport/index.ts
+++ WEB(BE)/src/middleware/passport/index.ts
@@ -1,10 +1,9 @@
 // passport에 기본적인 설정을 담아놓는 파일
-const passport = require('passport');
+import passport from "passport";
+import local from "./localStrategy";
+//const user = require("../models/index").models.user; // Sequelize ORM DB
 
-const local = require('./localStrategy');
-const user = require('../models/index').models.user;  // DB
-
-module.exports = () => {
+export default () => {
   // 7. 로그인시 실행되며, req.session에 데이터를 저장 즉, 사용자 정보를 세션에 아이디로 저장함.
   // 이때 아이디 키값만 저장(메모리 최적화를 위해서), 그 다음 passport.deserializeUser()로 바로 넘어감.
   passport.serializeUser((user, done) => {
@@ -14,9 +13,10 @@ module.exports = () => {
   // 8. DB 조회 후 req.user 객체를 등록 후 , done() 반환하여 다시 routes/login.js의 req.login 미들웨어로 다시 돌아간다.
   // 매 요청시 실행됨. 즉, 세션에 저장한 아이디를 통해 사용자 정보를 불러옴.
   passport.deserializeUser((us_id, done) => {
-    user.findOne({ where: { us_id } })
-      .then(user => done(null, user))
-      .catch(err => done(err));
+    user
+      .findOne({ where: { us_id } })
+      .then((user) => done(null, user))
+      .catch((err) => done(err));
   });
 
   local();
