82e5022f5cdd31a0a2e690d96c2f2f322615677d
asitis <asitisdev@gmail.com>
2022-10-10 10:30:01 +0900

    feat: 경계작전명령서 Precept 컴포넌트로 분리

--- WEB(FE)/src/pages/Dutyprecept.js
+++ WEB(FE)/src/pages/Dutyprecept.js
@@ -1,14 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Layout, PageHeader, Table, Row, Col, Typography } from "antd";
-import {
-  topTableColumns,
-  topTabledata,
-  middleTableColumns,
-  middleTabledata,
-  bottomTableColumns,
-  bottomTabledata,
-} from "./dutypreceptdummydata";
+import { Layout, PageHeader } from "antd";
+import Precept from "../components/Precept";
 
 const { Content } = Layout;
 
@@ -25,47 +18,7 @@ const Dutyprecept = () => {
           onBack={() => null}
           title={`경계작전명령서 (${params.date})`}
         />
-        <div
-          style={{
-            backgroundColor: "#ECEBE2",
-            padding: "1rem",
-          }}
-        >
-          <Row justify="center" gutter={[16, 16]}>
-            <Col xs={24} xl={22}>
-              <Typography.Title level={3}>지휘통제실</Typography.Title>
-              <Table
-                columns={topTableColumns}
-                dataSource={topTabledata}
-                pagination={{ position: ["none", "none"] }}
-              />
-            </Col>
-            <Col xs={24} xl={11}>
-              <Typography.Title level={3}>탄약고</Typography.Title>
-              <Table
-                columns={middleTableColumns}
-                dataSource={middleTabledata}
-                pagination={{ position: ["none", "none"] }}
-              />
-            </Col>
-            <Col xs={24} xl={11}>
-              <Typography.Title level={3}>CCTV</Typography.Title>
-              <Table
-                columns={middleTableColumns}
-                dataSource={middleTabledata}
-                pagination={{ position: ["none", "none"] }}
-              />
-            </Col>
-            <Col xs={24} xl={22}>
-              <Typography.Title level={3}>불침번</Typography.Title>
-              <Table
-                columns={bottomTableColumns}
-                dataSource={bottomTabledata}
-                pagination={{ position: ["none", "none"] }}
-              />
-            </Col>
-          </Row>
-        </div>
+        <Precept />
       </Content>
     </Layout>
   );
