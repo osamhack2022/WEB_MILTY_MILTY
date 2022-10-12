1292135c902bba14187d304720f4c1771481529d
Kim JaeJune <88999549+jaeiko@users.noreply.github.com>
2022-10-12 09:33:37 +0000

    feat: 근무 작성 로직 생성

--- WEB(BE)/src/controllers/make_duty.js
+++ WEB(BE)/src/controllers/make_duty.js
@@ -1,14 +1,20 @@
+
 const bcrypt = require('bcryptjs');
 const work = require('../models/work');
 const work_day = require('../models/work_day');
 
 exports.work = async function (req, res) {
-  let { } = req.body;
-};
-exports.work_day = async function (req, res) {
-  let { } = req.body;
-};
+  let {
 
-exports.duty = () => {
-  console.log('duty');
-};
+  } = req.body;
+}
+exports.work_day = async function (req, res) {
+  let {
+
+  } = req.body;
+}
+
+exports.duty = function () { };
+
+
+
