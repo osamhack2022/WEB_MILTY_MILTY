357b3feab63af0917ef7a80b5915704e3720aa1e
Kim JaeJune <88999549+jaeiko@users.noreply.github.com>
2022-10-12 09:32:27 +0000

    docs: 주석 설명 추가

--- WEB(BE)/src/app.js
+++ WEB(BE)/src/app.js
@@ -10,7 +10,7 @@ app.use(helmet());
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
-// #region SEQULIZE
+// #region SEQULIZE - connecting DB
 const { sequelize } = require('./models');
 
 sequelize
@@ -21,7 +21,6 @@ sequelize
   .catch(err => {
     console.error(`connection fail - ${err}`);
   });
-
 // #endregion
 
 // #region PASSPORT
@@ -40,17 +39,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 // #endregion
 
-// #region VIEW TESTING
-// 모든 라우트에서 실행되는 미들웨어
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.isAuthenticated(); // locals에 저장하면 view에서 바로 사용가능 / isAuthenticated() : 현재 로그인이 되어있는지 확인하는 함수
-  res.locals.currentUser = req.user;
-  next();
-});
+// // #region VIEW TESTING
+// // 모든 라우트에서 실행되는 미들웨어
+// app.use((req, res, next) => {
+//   res.locals.isAuthenticated = req.isAuthenticated(); // locals에 저장하면 view에서 바로 사용가능 / isAuthenticated() : 현재 로그인이 되어있는지 확인하는 함수
+//   res.locals.currentUser = req.user;
+//   next();
+// });
 
+// #region OTHER OPTIONS
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+// #endregion
 
 // #region ROUTES
 app.use(express.static(path.join(__dirname, '../../WEB(FE)/build')))
@@ -60,6 +61,7 @@ app.get('/', function (res, req) {
 app.use('/', require('./routes'));
 // #endregion
 
+// #region SERVER OPERATION
 const port = 5000;
 app.listen(port, function () {
   console.log(`server on! http://localhost:${port}`);
