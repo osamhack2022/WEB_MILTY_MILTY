5b016e712d6361a37156978fd0be02f4979af1ef
권종원 <73750927+KwonJongWon123@users.noreply.github.com>
2022-10-17 06:13:41 +0000

    chore: models/근무,열외 추가

---
+++ WEB(BE)/src/models/duty.model.js
@@ -0,0 +1,41 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Duty extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        duty_pid: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          allowNull: true,
+          autoIncrement: true,
+          unique: true,
+        },
+        usr_division_code: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          unique: true,
+        },
+        duty_name: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+          unique: false,
+        },
+        duty_people_num: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          unique: false,
+        }
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: false,
+        tableName: 'duty',
+        paranoid: false,
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+      },
+    );
+  }
+};
---
+++ WEB(BE)/src/models/exempt.model.js
@@ -0,0 +1,46 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Exempt extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        exempt_pid: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          allowNull: true,
+          autoIncrement: true,
+          unique: true,
+        },
+        usr_pid: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          unique: true,
+        },
+        exempt_start: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          unique: false,
+        },
+        exempt_end: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          unique: false,
+        },
+        exempt_list: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+          unique: false,
+        }
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: false,
+        tableName: 'exempt',
+        paranoid: false,
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+      },
+    );
+  }
+};
