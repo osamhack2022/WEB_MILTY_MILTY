62d177103514bbf9e2ef2284df2dec17d066a084
asitis <asitisdev@gmail.com>
2022-10-17 19:58:03 +0900

    refactor: 불필요한 코드 삭제

--- WEB(FE)/src/App.js
+++ WEB(FE)/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -7,37 +7,16 @@ import Soldier from "./pages/Soldier";
 import Main from "./pages/Main";
 import "./App.less";
 
-const App = () => {
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  const handleResize = () => {
-    setWindowSize({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/main" element={<Main />} />
-        <Route exact path="/info" render={() => <Info userInfo={userInfo} />} />
-        <Route path="/admin/*" element={<Admin />} />
-        <Route path="/soldier/*" element={<Soldier />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/main" element={<Main />} />
+      <Route path="/admin/*" element={<Admin />} />
+      <Route path="/soldier/*" element={<Soldier />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
