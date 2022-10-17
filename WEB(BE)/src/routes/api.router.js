680798c2355701cab8701b83d00cda63929afc94
asitis <asitisdev@gmail.com>
2022-10-17 19:46:10 +0900

    fix: 로그인 API 오타 수정

--- WEB(BE)/src/routes/api.router.js
+++ WEB(BE)/src/routes/api.router.js
@@ -15,28 +15,28 @@ router.post(
   function (req, res) {
     // If this function gets called, authentication was successful.
     // `req.user` contains the authenticated user.
-    console.log('로그인 성공!',
+    console.log(
+      '로그인 성공!',
       '아이디 :', req.user.usr_id,
       '이름 :', req.user.usr_name,
       '생일 :', req.user.usr_birthday,
       '부대 :', req.user.usr_division,
       '부대 코드: ', req.user.usr_division_code,
       '신분: ', req.user.usr_class,
-      '전역일: ', req.user.usr_discharge_date);
-    res.status(200).json(
-      {
-        "result": "success",
-        "user": {
-          "user_id": req.user.usr_id,
-          "user_name": req.user.usr_name,
-          "uesr_birthday": req.user.usr_birthday,
-          "user_division": req.user.usr_division,
-          "user_division_code": req.user.usr_division_code,
-          "user_class": req.user.usr_class,
-          "user_discharge_date": req.user.usr_discharge_date
-        }
-      }
+      '전역일: ', req.user.usr_discharge_date,
     );
+    res.status(200).json({
+      result: 'success',
+      user: {
+        user_id: req.user.usr_id,
+        user_name: req.user.usr_name,
+        user_birthday: req.user.usr_birthday,
+        user_division: req.user.usr_division,
+        user_division_code: req.user.usr_division_code,
+        user_class: req.user.usr_class,
+        user_discharge_date: req.user.usr_discharge_date,
+      },
+    });
 
     /*
     res.status(200).json({ classification: 'admin' });  //관리자 페이지로 이동
@@ -61,6 +61,5 @@ router.post('/make_duty', duty); // 근무 생성 데이터 받는 곳
 // 근무변경 및 건의사항
 router.post('/user-request-set', user_request); // 근무변경 및 건의사항 정보 넣기 / 하는중
 router.post('/user-request-get', user_request); // 근무변경 및 건의사항 정보 받기 / 하고있는중
-
 
 module.exports = router;
