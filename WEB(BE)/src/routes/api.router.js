af7c6969098b4f756bad3e9f40eddd08ac6d7406
Kim JaeJune <88999549+jaeiko@users.noreply.github.com>
2022-10-17 12:27:15 +0000

    refactor: duty 관련 모듈 추가

--- WEB(BE)/src/routes/api.router.js
+++ WEB(BE)/src/routes/api.router.js
@@ -3,9 +3,11 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const { register } = require('../controllers/auth.controller');
-const { duty } = require('../controllers/set_duty');
+const { set_duty, get_duty, set_duty_timeslot, get_duty_timeslot, set_duty_schedule, get_duty_schedule } = require('../controllers/duty');
+const { get_check_count } = require('../controllers/check_count')
 const { user_request } = require('../controllers/user_request');
 
+// #### Auth region ####
 // login 데이터 받는 곳
 router.post(
   '/login',
@@ -54,14 +56,37 @@ router.get('/logout', function (req, res) {
 
 // 회원가입 진행
 router.post('/register', register); // register 데이터 받는 곳
+// #### End region ####
 
+
+// #### Duty region ####
 // 근무 생성
-router.post('/set-duty', duty); // 근무 생성 데이터 받는 곳
+router.post('/set-duty', set_duty); // 근무 생성 데이터 받는 곳
 
 // 유저 근무 확인 ( 한달안에 내 근무가 언제있는지)
+router.post('/get-duty', get_duty);
 
+// 근무 시간대 생성
+router.post('/set-duty-timeslot', set_duty_timeslot);
+
+// 근무 시간대 조회
+router.post('/get-duty-timeslot', get_duty_timeslot);
+
+// 해당 날짜의 근무표 생성
+router.post('/set-duty-schedule', set_duty_schedule);
+
+// 해당 날짜의 근무표 조회
+router.post('/get-duty-schedule', get_duty_schedule);
+
+// 근무 횟수 조회
+router.post('/get-check-count', get_check_count);
+// #### End region ####
+
+
+// #### Request region ####
 // 근무변경 및 건의사항
 router.post('/user-request-set', user_request); // 근무변경 및 건의사항 정보 넣기 / 하는중
 router.post('/user-request-get', user_request); // 근무변경 및 건의사항 정보 받기 / 하고있는중
+// #### End region ####
 
 module.exports = router;
