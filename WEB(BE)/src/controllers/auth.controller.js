a66e5947d21669a99fec474f70dc659aeaa13904
Kim JaeJune <88999549+jaeiko@users.noreply.github.com>
2022-10-18 08:08:36 +0000

    feat: 관리자 권한으로 체크해서 가입했을 때의 코드 추가

--- WEB(BE)/src/controllers/auth.controller.js
+++ WEB(BE)/src/controllers/auth.controller.js
@@ -14,6 +14,7 @@ exports.register = async function (req, res) {
     user_division_code,
     user_class,
     user_discharge_date,
+    checked
   } = req.body;
   // 비밀번호 암호화
   user_password = bcrypt.hashSync(user_password);
@@ -21,7 +22,8 @@ exports.register = async function (req, res) {
   // 기존 아이디 존재 확인
   const id = await Users.findOne({ where: { usr_id: user_id } });
   console.log('########## ID :  ', id, '######## \n');
-  if (id == null) {
+  // if (id == null && checked == false)
+  if (id == null) {   // 가입할 때 이미 존재하는 id인지, 그리고 관리자 권한을 체크 안했는지 확인되면 병사로 가입
     // 존재하지 않으면 회원가입 저장
     let user = Users.create({
       usr_name: user_name,
@@ -31,11 +33,30 @@ exports.register = async function (req, res) {
       usr_division: user_division,
       usr_division_code: user_division_code,
       usr_class: user_class,
+      classification: false,
       usr_discharge_date: user_discharge_date,
     })
       .then(() => {
         return res.status(200).json('register success');
         // res.send('SUCCESS');
+      })
+      .catch(err => {
+        throw err;
+      });
+  } else if (id == null && checked == true) {    // 가입할 때 이미 존재하는 id인지, 그리고 관리자 권한을 체크 했는지 확인되면 관리자로 가입 요청(추후 DB에서 검토 후 classification: true로 바꾸면 관리자로 로그인)
+    Users.create({
+      usr_name: user_name,
+      usr_id: user_id,
+      usr_password: user_password,
+      usr_birthday: user_birthday,
+      usr_division: user_division,
+      usr_division_code: user_division_code,
+      usr_class: user_class,
+      classification: null,
+      usr_discharge_date: user_discharge_date,
+    })
+      .then(() => {
+        return res.status(200).json('register request completed');
       })
       .catch(err => {
         throw err;
