8e30abd3b43b860baa91bd07146e2969f3bba58e
JinJjaHaMa <69956347+hippo0419@users.noreply.github.com>
2022-10-23 07:53:55 +0000

    feat: user_discharge_date 리턴 형식 변경

--- WEB(BE)/src/routes/api.router.js
+++ WEB(BE)/src/routes/api.router.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const moment = require('moment');
+const passport = require('passport');
 
 const router = express.Router();
-const passport = require('passport');
 const { register } = require('../controllers/auth.controller');
 const {
   set_duty,
@@ -60,7 +61,7 @@ router.post(
         user_division: req.user.usr_division,
         user_division_code: req.user.usr_division_code, // user_division_code 를 통해 특정 부대의 페이지에만 접근 가능하도록 설정
         user_class: req.user.usr_class,
-        user_discharge_date: req.user.usr_discharge_date,
+        user_discharge_date: moment(req.user.usr_discharge_date).format('YYYY-MM-DD'),
         classification: req.user.classification, // 1이면 Front 라우터에서 admin 페이지로, 2이면 user main 페이지로, null이면 로그인 못하게!
       },
     });
