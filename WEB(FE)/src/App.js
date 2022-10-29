cde9e4ff6828784842ee1f0da4a7524ba50fb31c
asitis <asitisdev@gmail.com>
2022-10-29 10:01:28 +0900

    fix: 로그인시 무한루프에 빠지는 버그 수정

--- WEB(FE)/src/App.js
+++ WEB(FE)/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
           </ProtectedRoute>
         }
       />
-      <Route exact
+      <Route
         index
         element={
           user === null ? (
--- WEB(FE)/src/components/ProtectedRoute.js
+++ WEB(FE)/src/components/ProtectedRoute.js
@@ -3,15 +3,14 @@ import { useAuth } from "../hooks/useAuth";
 
 const ProtectedRoute = ({ classification, children }) => {
   const { user } = useAuth();
-  console.log("api", user);
   if (!user) {
     // user is not authenticated
     return <Navigate to="/" />;
   }
   // 일단은 반환된 classification으로 값을 비교하지만 나중에는 /api/authtoken으로 토큰 검증후 페이지이동을 구현 시킬 예정.
-  if (!user.token || user.classification !== classification) {
-    return <Navigate to="/" />
-  }
+  // if (!user.token || user.classification !== classification) {
+  //   return <Navigate to="/" />;
+  // }
   return children;
 };
 
