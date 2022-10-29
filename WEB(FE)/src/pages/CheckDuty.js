ea4aa222accc0e9d493a9c4cdf630d43f42092db
asitis <asitisdev@gmail.com>
2022-10-29 15:10:07 +0900

    feat: 근무 확인 페이지 API 연동 코드 작성

--- WEB(FE)/src/pages/CheckDuty.js
+++ WEB(FE)/src/pages/CheckDuty.js
@@ -1,16 +1,64 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Layout, Badge, Button, PageHeader } from "antd";
+import { useNavigate } from "react-router-dom";
+import { Layout, Tag, Button, PageHeader } from "antd";
 import axios from "axios";
 import { useAuth } from "../hooks/useAuth";
 import CustomCalendar from "../components/CustomCalendar";
 
 const { Content } = Layout;
 
+const schedule = [
+  {
+    pid: 1,
+    date: "2022-10-08",
+    duty_name: "무기고",
+    start_time: "08:00",
+    end_time: "12:00",
+  },
+  {
+    pid: 2,
+    date: "2022-10-15",
+    duty_name: "위병소",
+    start_time: "12:00",
+    end_time: "16:00",
+  },
+  {
+    pid: 3,
+    date: "2022-10-15",
+    duty_name: "CCTV",
+    start_time: "20:00",
+    end_time: "22:00",
+  },
+  {
+    pid: 4,
+    date: "2022-10-17",
+    duty_name: "CCTV",
+    start_time: "20:00",
+    end_time: "22:00",
+  },
+];
+
+const color = (name) => {
+  switch (name) {
+    case "CCTV":
+      return "green";
+    case "무기고":
+      return "gold";
+    case "불침번":
+      return "geekblue";
+    case "위병소":
+      return "purple";
+    case "당직":
+      return "magenta";
+    default:
+      return "default";
+  }
+};
+
 const CheckDuty = () => {
   const navigate = useNavigate();
   const { user } = useAuth();
-  const [schedule, setSchedule] = useState([]);
+  // const [schedule, setSchedule] = useState([]);
 
   const fetchUserDutySchedule = useCallback(() => {
     axios
@@ -19,18 +67,7 @@ const CheckDuty = () => {
       })
       .then((response) => {
         if (response.status === 200 && response.data.result === "success") {
-          setSchedule(
-            response.data.schedule.map(
-              ({ pid, date, duty_name, start_time, end_time }) => ({
-                key: pid,
-                index: pid,
-                date,
-                duty_name,
-                start_time,
-                end_time,
-              })
-            )
-          );
+          setSchedule(response.data.schedule);
         }
       })
       .catch((error) => {
@@ -38,40 +75,25 @@ const CheckDuty = () => {
       });
   }, [user]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    //   fetchUserDutySchedule();
+  }, []);
 
   const dateCellRender = useCallback(
-    (date) => {
-      const data = schedule.filter((item) => date.isSame(item.date, "day"));
-
-      return (
-        <div>
-          <ul className="events">
-            {data.map((item) => (
-              <li key={item.key}>
-                <Badge
-                  style={{}}
-                  color={(() => {
-                    let color;
-                    if (item.isDone) color = "green";
-                    else if (item.isSigned) color = "geekblue";
-                    else color = "red";
-
-                    return color;
-                  })()}
-                  text={`${item.name} ${item.startTime}-${item.endTime}`}
-                />
-              </li>
-            ))}
-          </ul>
-          {data.length === 0 ? null : (
-            <Link to={`${date.format("YYYY-MM-DD")}`}>
-              <Button href={`${date.format("YYYY-MM-DD")}`}>경작서 확인</Button>
-            </Link>
-          )}
-        </div>
-      );
-    },
+    (date) => (
+      <div>
+        {schedule
+          .filter((item) => date.isSame(item.date, "day"))
+          .map((item) => (
+            <Button type="text" key={item.pid} style={{ padding: "0" }}>
+              <Tag color={color(item.duty_name)}>{item.duty_name}</Tag>
+              <span>
+                {item.start_time}-{item.end_time}
+              </span>
+            </Button>
+          ))}
+      </div>
+    ),
     [schedule]
   );
 
